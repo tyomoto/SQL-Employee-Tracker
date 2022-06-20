d943c9996d9260f98190f5cfc03f206475005ff3
T Omoto <tyler.omoto@gmail.com>
2022-06-20 14:17:02 -0400

    Created database and tables in schema sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,27 @@
 DROP DATABASE IF EXISTS employeetrackerDB;
 CREATE database employeetrackerDB;
 
-USE database employeetrackerDB;
+USE employeetrackerDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL (10, 2),
+    department_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name: VARCHAR(30) NULL,
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+)
